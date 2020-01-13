37f3b29a012ce7bac68b9af1adbfbb8bbd34d31b
Paola <Paola@DESKTOP-8O65BSH.station>
2020-01-13 17:14:42 +0100

    Aggiunti Metodi Standard

--- src/it/polito/tdp/LibrettoUniversitario/Voto.java
+++ src/it/polito/tdp/LibrettoUniversitario/Voto.java
@@ -1,5 +1,35 @@
 package it.polito.tdp.LibrettoUniversitario;
 
-public class Voto {
+import java.time.LocalDate;
 
+public class Voto {
+	private int Voto;
+	private String Corso;
+	private LocalDate Data;
+	public Voto(int voto, String corso, LocalDate data) {
+		super();
+		Voto = voto;
+		Corso = corso;
+		Data = data;
+	}
+	public int getVoto() {
+		return Voto;
+	}
+	public void setVoto(int voto) {
+		Voto = voto;
+	}
+	public String getCorso() {
+		return Corso;
+	}
+	public void setCorso(String corso) {
+		Corso = corso;
+	}
+	public LocalDate getData() {
+		return Data;
+	}
+	public void setData(LocalDate data) {
+		Data = data;
+	}
+
+	
 }
